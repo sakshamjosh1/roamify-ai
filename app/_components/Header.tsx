93828745aa7cbe030c408a6ed53483527c1373e4
Saksham Joshi <150180744+sakshamjosh1@users.noreply.github.com>
2025-08-13 19:59:33 +0530

    Initial Commit

---
+++ app/_components/Header.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { SignInButton } from '@clerk/nextjs';
+
+const menuOptions=[
+    {
+        name:'Home',
+        path:'/'
+    },
+    {
+        name:'Pricing',
+        path:'/pricing'
+    },
+    {
+        name:'Contact us',
+        path:'/contact-us'
+    },
+]
+
+function Header() {
+  return (
+    <div className='flex justify-between items-center p-4'>
+       {/* Logo */}
+       <div className='flex gap-2 items-center'>
+           <Image src={'/logo.png'} alt='logo' width={40} height={40}/>
+           <h2 className='font-bold text-2xl'>RoamifyAI</h2>
+       </div>
+       
+       {/* Menu Options */}
+       <div className='flex gap-8 items-center '>
+          {menuOptions.map((menu, index)=>(
+              <Link key={menu.path} href={menu.path}>
+                  <h2 className='font-bold text-lg hover:scale-105 transition-all hover:text-primary'>{menu.name}</h2>
+              </Link>
+            
+          ))}
+       </div>
+
+       {/* Get Started */}
+       <SignInButton mode='modal'>
+          <Button>Get Started</Button>
+       </SignInButton>
+       
+    </div>
+  )
+}
+
+export default Header;
