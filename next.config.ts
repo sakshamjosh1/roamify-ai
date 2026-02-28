ab31f8d673e4ff53e1c27318be984d032fea9a74
Saksham Joshi <150180744+sakshamjosh1@users.noreply.github.com>
2026-02-28 15:07:46 +0530

    few changes

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: [
-      "places.googleapis.com",
-      "images.unsplash.com",
-      "plus.unsplash.com",
-    ],
-  },
+images: {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'images.unsplash.com',
+    },
+    {
+      protocol: 'https',
+      hostname: 'lh3.googleusercontent.com',
+    },
+    {
+      protocol: 'https',
+      hostname: '**',
+    },
+    {
+      protocol: 'https',
+      hostname: 'maps.googleapis.com',
+    },
+    {
+      protocol: 'https',
+      hostname: 'maps.gstatic.com',
+    }
+  ],
+},
 };
 
 export default nextConfig;
